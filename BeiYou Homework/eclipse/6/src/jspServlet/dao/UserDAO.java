3a768604ef4ece16549087553c0eb769c81c0951
cui_yaji_001447324 <cui.yaj@husky.neu.edu>
2019-02-15 20:18:25 -0500

    upoad 2

--- BeiYou Homework/eclipse/6/src/jspServlet/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package jspServlet.dao;
-
-import jspServlet.vo.User;
-
-public interface UserDAO {
-	public int queryByUsername(User user) throws Exception;
-	public int insertUser(User user) throws Exception;
-	public int updateUser(User user)throws Exception;
-}
